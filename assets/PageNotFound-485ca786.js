8d455728f2f0b9f3f21ea07e8cd9c5bb96bb746c
natali1503 <nfv1503@gmail.com>
2024-01-24 08:26:11 +0500

    Updates

--- assets/PageNotFound-485ca786.js
+++
@@ -1,1 +0,0 @@
-import{j as n}from"./index-56d039c7.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
---
+++ assets/PageNotFound-b488114b.js
@@ -0,0 +1,1 @@
+import{j as n}from"./index-8f762a23.js";function t(){return n.jsx("div",{children:n.jsx("h1",{children:"Page not found 😢"})})}export{t as default};
